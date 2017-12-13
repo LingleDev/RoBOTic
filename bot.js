f5cb9143b27e2b7c957763217f044bb5275e527d
FHGDev <gideon.lingle14@gmail.com>
2017-12-13 11:32:47 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,6 @@
 const d = require('discord.js');
 const bot = new d.Client();
 const prefix = process.env.prefix
-const fs = require('fs')
-
-bot.commands = new d.Collection();
-
-require('fs').readdir("./commands/", (err, files) => {
-  console.log("Loading commands...")
-  if (err) return console.log(`Command loading failed!`);
-  files.filter(f => f.split(".").pop() === "js").forEach((f, i) => {
-    bot.commands.set(require(`./commands/${f}`).help.name, require(`./commands/${f}`));
-  })
-})
 
 bot.on("ready", ready => {
   console.log("RoBOTic Version 0.0.1 Loaded!")
@@ -22,6 +11,9 @@ bot.on("ready", ready => {
 bot.on("message", message => {
   if (message.content == prefix + "help") {
     message.channel.send("Help and ping! :grin:")
+  }
+  if (message.content == prefix + "ping") {
+    message.channel.send(`:ping_pong: PONG! My ping is ${bot.ping} ms!`)
   }
 })
 
