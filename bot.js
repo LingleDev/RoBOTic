89b733a46856a5b89140d8e4f13e28507bf4c78e
FHGDev <gideon.lingle14@gmail.com>
2017-12-13 11:10:11 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,10 +3,21 @@ const bot = new d.Client();
 const prefix = process.env.prefix
 const fs = require('fs')
 
+bot.commands = new d.Collection();
+
+require('fs').readdir("./commands/", (err, files) => {
+  console.log("Loading commands...")
+  if (err) return console.log(`Command loading failed!`);
+  files.filter(f => f.split(".").pop() === "js").forEach((f, i) => {
+    bot.commands.set(require(`./commands/${f}`).help.name, require(`./commands/${f}`));
+  })
+})
+
 bot.on("ready", ready => {
   console.log("RoBOTic Version 0.0.1 Loaded!")
   bot.user.setGame("j!help | Serving the owner.")
 })
+
 
 bot.on("message", message => {
   if (message.content == prefix + "help") {
