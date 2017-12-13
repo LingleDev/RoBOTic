c6213231c69ba0b0d2e7eb1fa02a7dafac157911
FHGDev <gideon.lingle14@gmail.com>
2017-12-13 11:21:11 -0600

    Update reply.js

--- commands/reply.js
+++ commands/reply.js
@@ -1,6 +1,3 @@
-const prefix = require('../config.json').prefix
-const owner = require('..config.json').james
+//module.exports.run = (bot, message, args) => {
 
-module.exports.run = (bot, message, args) => {
-  
-}
+//}
