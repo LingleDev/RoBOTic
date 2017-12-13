b10c9dd8f63c6fb4869ce3b3a150f2496a3202c1
FHGDev <gideon.lingle14@gmail.com>
2017-12-13 12:09:08 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,53 @@ bot.on("ready", ready => {
 
 bot.on("message", message => {
   if (message.content == prefix + "help") {
-    message.channel.send("Help and ping! :grin:")
+    const embed = {
+  "title": "RoBOTic Help",
+  "description": "You asked for help, here it is! :stuck_out_tongue:",
+  "url": "https://discord.gg/wxrWNWZ",
+  "color": 508910,
+  "timestamp": "2017-12-13T17:51:29.259Z",
+  "footer": {
+    "icon_url": "https://cdn.discordapp.com/attachments/349661784057184256/390558851348561945/bot.png",
+    "text": "footer text"
+  },
+  "thumbnail": {
+    "url": "https://cdn.discordapp.com/attachments/349661784057184256/390558851348561945/bot.png"
+  },
+  "image": {
+    "url": "https://cdn.discordapp.com/attachments/349661784057184256/390558851348561945/bot.png"
+  },
+  "author": {
+    "name": "RoBOTic",
+    "url": "",
+    "icon_url": "https://cdn.discordapp.com/attachments/349661784057184256/390558851348561945/bot.png"
+  },
+  "fields": [
+    {
+      "name": "Utility",
+      "value": "A ban command is coming soon."
+    },
+    {
+      "name": "Bot Status",
+      "value": "Ping"
+    },
+    {
+      "name": "Misc",
+      "value": "Help"
+    },
+    {
+      "name": "Bot Developer",
+      "value": "FreakingHulk Gaming#6545",
+      "inline": true
+    },
+    {
+      "name": "Bot Owner",
+      "value": "🎁JamesRobotic🎁13#1840",
+      "inline": true
+    }
+  ]
+};
+message.channel.send({embed})
   }
   if (message.content == prefix + "ping") {
     message.channel.send(`:ping_pong: PONG! My ping is ${bot.ping} ms!`)
