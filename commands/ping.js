164faff38ab300eaebfc4d8e9967bb62e786af2d
FHGDev <gideon.lingle14@gmail.com>
2017-12-13 11:19:20 -0600

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,3 @@
-const discord = require('discord.js')
-const config = require('../config.json')
-const prefix = config.prefix
 
 module.exports.run = (bot, message, args) => {
     message.channel.send(":ping_pong: PONG! My ping is " + bot.ping + "ms!")
