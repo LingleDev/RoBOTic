d5e0c734a26fb5820e30b9e11723b5a1e1a51978
FHGDev <gideon.lingle14@gmail.com>
2017-12-13 11:11:03 -0600

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -3,9 +3,7 @@ const config = require('../config.json')
 const prefix = config.prefix
 
 module.exports.run = (bot, message, args) => {
-  if (message.content == prefix + "ping") {
     message.channel.send(":ping_pong: PONG! My ping is " + bot.ping + "ms!")
-  }
 }
 
 module.exports.help = {
