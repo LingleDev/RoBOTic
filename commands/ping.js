9ba623b198aacf52f004fa09bd0003eda1a3d1dc
FHGDev <gideon.lingle14@gmail.com>
2018-01-17 11:19:34 -0600

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,9 +1,9 @@
 const discord = require('discord.js')
 
-module.exports.run = (bot, message, args) => {
+module.exports = (bot, message, args) => {
   let embed = new discord.RichEmbed()
   .setTitle(`${bot.user.username} Ping`)
-  .setDescription(`:ping_pong: PONG!!!! My heartbeat is ${bot.ping}, and my memory usage is ${}`)
+  .setDescription(`:ping_pong: PONG!!!! My heartbeat is ${bot.ping}, and my memory usage is ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB.`)
 }
 
 module.exports.help = {
