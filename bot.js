7f370bc1db88d0586c06786e769b2853075a3569
FHGDev <gideon.lingle14@gmail.com>
2018-04-25 20:19:28 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,78 +1,38 @@
-const d = require('discord.js');
-const bot = new d.Client();
+const discord = require('discord.js');
+const bot = new discord.Client();
 const prefix = process.env.prefix
 
-
-bot.on("ready", () => {
-  console.log("RoBOTic Version 0.0.1 Loaded!")
-  bot.user.setGame(`RoBOTic Version 0.0.1 Loaded!`)
-  setInterval(() => {
-    bot.user.setGame(`j!help | ${bot.guilds.array().length} server(s)`)
-  }, 2500)
- })
-
-bot.on("message", message => {
-  let args = message.content.split(' ').slice(0)
-  
-  bot.commands = new d.Collection();
+bot.commands = new discord.Collection();
 
 require('fs').readdir("./commands/", (err, files) => {
   console.log("Loading commands...");
   if (err) return console.log(`Command loading failed!`);
   files.filter(f => f.split(".").pop() === "js").forEach((f, i) => {
-    bot.commands.set(require(`./commands/${f}`).help.name, require(`./commands/${f}`)(bot, message, args));
+    bot.commands.set(require(`./commands/${f}`).help.name, require(`./commands/${f}`));
   });
 });
 
-  if (message.content == prefix + "help") {
-    const embed = {
-  "title": "RoBOTic Help",
-  "description": "You asked for help, here it is! :stuck_out_tongue:",
-  "url": "https://discord.gg/wxrWNWZ",
-  "color": 508910,
-  "timestamp": new Date(),
-  "footer": {
-    "icon_url": "https://cdn.discordapp.com/attachments/349661784057184256/390558851348561945/bot.png",
-    "text": "RoBOTic Help Menu"
-  },
-  "thumbnail": {
-    "url": "https://cdn.discordapp.com/attachments/349661784057184256/390558851348561945/bot.png"
-  },
-  "image": {
-    "url": "https://cdn.discordapp.com/attachments/349661784057184256/390558851348561945/bot.png"
-  },
-  "author": {
-    "name": "RoBOTic",
-    "url": "",
-    "icon_url": "https://cdn.discordapp.com/attachments/349661784057184256/390558851348561945/bot.png"
-  },
-  "fields": [
-    {
-      "name": "Utility",
-      "value": "A ban command is coming soon."
-    },
-    {
-      "name": "Bot Status",
-      "value": "Ping"
-    },
-    {
-      "name": "Misc",
-      "value": "Help"
-    },
-    {
-      "name": "Bot Developer",
-      "value": "FreakingHulk Gaming#6545",
-      "inline": true
-    },
-    {
-      "name": "Bot Owner",
-      "value": "🎁JamesRobotic🎁13#1840",
-      "inline": true
-    }
-  ]
-};
-message.channel.send({embed}).then(m => m.delete(15000))
-  }
-})
+bot.on("ready", () => {
+  console.log("RoBOTic Version 1.0.0 Loaded!")
+  bot.user.setActivity(`Loading RoBOTic Version 1.0.0 Loaded!`, {type: "STREAMING", url: "https://youtube.com/c/FreakingHulkGaming"})
+  setTimeout(err => {
+    if (err) return console.error(err)
+    bot.user.setActivity(`for j!help | ${bot.guilds.size} server(s)`, {type: "WATCHING"})
+  }, 10000)
+ })
 
+bot.on("message", message => {
+  if (message.channel.type == "dm") return;
+  let mArray = message.content.split(" ")
+  let args = mArray.slice(1)
+  let cmd = bot.commands.get(mArray.slice(prefix.length))
+  
+  if (cmd) {
+    cmd.run(bot, message, args, discord)
+    console.log(`${message.member.username} used the ${message.content.split(" ")[0]} command.`)
+  }
+});
+
+  
+  
 bot.login(process.env.tok)
